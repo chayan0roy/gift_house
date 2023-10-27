281585850346fc1483f0d717758b09cd9f6ea10e
ChayanRoy <chayanthe01guy@gmail.com>
2023-10-27 18:43:40 +0530

    Second update

--- server/index.js
+++ server/index.js
@@ -201,7 +201,7 @@ app.get('/search/:key', async (req, res) => {
 
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log("Server Start");
 });
